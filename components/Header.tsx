1d2480e881ee1e4204635e6474f73031bb9634df
Legit-Sam <webdeveloper2167@gmail.com>
2024-10-21 18:58:53 +0100

    Finanl

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,26 +12,28 @@ import Link from "next/link";
 export function Header() {
   return (
     <div className="relative z-10 border-b py-4 bg-gray-50 w-full">
-      <div className="items-center w-full px-4 justify-between flex">
-        <Link href="/" className="flex gap-2 items-center text-xl text-black">
-          <Image src="/logo.png" width="50" height="50" alt="file drive logo" />
-          FileDrive
-        </Link>
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between">
+          <Link href="/" className="flex gap-2 items-center text-xl text-black">
+            <Image src="/logo.png" width="50" height="50" alt="file drive logo" />
+            FileDrive
+          </Link>
 
-        <SignedIn>
-          <Button variant={"outline"}>
-            <Link href="/dashboard/files">Your Files</Link>
-          </Button>
-        </SignedIn>
+          <SignedIn>
+            <Button variant={"outline"}>
+              <Link href="/dashboard/files">Your Files</Link>
+            </Button>
+          </SignedIn>
 
-        <div className="flex gap-2">
-          <OrganizationSwitcher />
-          <UserButton />
-          <SignedOut>
-            <SignInButton>
-              <Button>Sign In</Button>
-            </SignInButton>
-          </SignedOut>
+          <div className="flex gap-2">
+            <OrganizationSwitcher />
+            <UserButton afterSignOutUrl="/" />
+            <SignedOut>
+              <SignInButton>
+                <Button>Sign In</Button>
+              </SignInButton>
+            </SignedOut>
+          </div>
         </div>
       </div>
     </div>
